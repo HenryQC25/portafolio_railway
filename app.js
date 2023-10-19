ae1155ef63063b7ebd30382648bd80556019d204
HenryQC25 <hquenuncruztic@gmail.com>
2023-10-18 18:37:41 -0600

    version4  funcionando

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const hbs=require('hbs') //agregamos hbs
+require('dotenv').config();//agregamos configuracion dotenv
+const app = express()
+const port = process.env.PORT //configuramos el puerto
+
+//utilizacion de hbs para el renderizado de la pagina web
+app.set('view engine','hbs')
+
+//uso de parciales "codigo que se repite en ,as de un lugar o paginas web"
+//agregamos partials
+hbs.registerPartials(__dirname +'/views/partials')
+
+//contenido estatico donde esta template
+app.use(express.static('public'))
+
+
+app.get('/',(req,res)=>{
+   res.render('index',{
+    nombre: 'Henry Quenun',
+    titulo: 'curso node'
+   });
+})
+
+app.get('/bio',(req,res)=>{
+  res.render('bio',{
+    nombre: 'Henry Quenun',
+    titulo: 'curso node'
+   });
+})
+
+//para servir en la ruta localhost:8080/hola-mundo
+//app get para habilitar rutas
+app.get('/contacto',(req,res)=>{
+    res.render('contacto',{
+    nombre: 'Henry Quenun',
+    titulo: 'curso node'
+   });
+})
+
+app.get('/habilidades',(req,res)=>{
+    res.render('habilidades',{
+    nombre: 'Henry Quenun',
+    titulo: 'curso node'
+   });
+})
+
+app.get('/proyectos',(req,res)=>{
+    res.render('proyectos',{
+    nombre: 'Henry Quenun',
+    titulo: 'curso node'
+   });
+})
+
+app.get('*', (req, res) => {
+  res.render('404')
+});
+
+app.listen(port,()=>{
+    console.log(`ejemplo listen ${port}`);
+})
